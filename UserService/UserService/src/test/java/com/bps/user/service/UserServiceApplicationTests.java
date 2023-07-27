fc081fa21026da2e4b03679f3343f130c215c1fb
bhanu <pratapbhanu6@gmail.com>
2023-07-27 17:52:32 -0500

    Initial Commit

--- UserService/UserService/src/test/java/com/bps/user/service/UserServiceApplicationTests.java
+++ UserService/UserService/src/test/java/com/bps/user/service/UserServiceApplicationTests.java
@@ -1,6 +1,9 @@
 package com.bps.user.service;
 
+import com.bps.user.service.entities.Rating;
+import com.bps.user.service.external.services.RatingService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +12,12 @@ class UserServiceApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+//@Autowired
+	//private RatingService ratingService;
+	//@Test
+	//void createRating(){
+	//	Rating rating  = Rating.builder().rating(10).userId("").hotelId("").feedback("Awesome Service- this is created by feign client").build();
+     //   Rating savedRating = ratingService.createRating(rating);
+	//	System.out.println("new rating created");
+//}
 }
